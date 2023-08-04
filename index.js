e1f4a76b93a8ad98d43256b810b7e02504bdb289
unknown <nabilasoma@gmail.com>
2023-08-04 19:08:33 +0600

    added some functions for crud oparetaions

---
+++ index.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5rfymji.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const taskCollection = client.db('taskDB').collection('task');
+
+
+    app.get('/task', async(req, res) => {
+        const cursor = taskCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    });
+
+
+    app.get('/task/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await taskCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post('/task', async(req, res) => {
+        const newTask  = req.body;
+        console.log(newTask);
+        const result = await taskCollection.insertOne(newTask);
+        res.send(result);
+    });
+
+
+    app.put('/task/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updateDoc = req.body;
+      const updatedDoc = {
+        $set: {
+          title: updateDoc.title,
+          description: updateDoc.description,
+          status: updateDoc.status
+        },
+      };
+      const result = await taskCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
+    })
+
+    app.delete('/task/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Task management server is running');
+});
+
+
+app.listen(port, () => {
+    console.log(`Task management is running on prot: ${port}`);
+})
